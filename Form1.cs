9df6c0079ac32c3c5d533d0554cce8e1bd452bd3
GiorgosDrivas <78447254+GiorgosDrivas@users.noreply.github.com>
2022-02-17 20:36:12 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectionToDB
+{
+
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Connected!Check your Console output!";
+            string server = "localhost";
+            string database = "example";
+            string username = "root";
+            string password = "";
+            string constring = "SERVER=" + server + ";" + "DATABASE=" + database + ";"
+                + "UID=" + username + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection conn = new MySqlConnection(constring);
+            conn.Open();
+            string query = "select * from users";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["id"]);
+                Console.WriteLine(reader["username"]);
+                Console.WriteLine(reader["password"]);
+                Console.WriteLine(reader["Job"]);
+            }
+            
+        }
+
+    }
+}
